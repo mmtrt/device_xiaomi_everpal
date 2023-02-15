636412ab2aad2cd325ce0a4e74a0897924c71b84
Sushrut1101 <guptasushrut@gmail.com>
2023-02-15 23:54:23 +0530

    everpal: Build the `Aperture` camera
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -119,6 +119,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
     android.hardware.camera.provider@2.6.vendor:64
 
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Charger
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
