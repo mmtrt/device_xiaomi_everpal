f2d29f7f51c7901cc17f2080561a1fe0519a45a1
Taqi Raza <mttalpur@gmail.com>
2023-02-18 19:51:24 +0500

    everpal: move nfc manifest under configs/vintf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,7 @@ BOARD_USES_METADATA_PARTITION := true
 
 # NFC
 ODM_MANIFEST_SKUS += nfc
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/nfc/manifest_nfc.xml
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
