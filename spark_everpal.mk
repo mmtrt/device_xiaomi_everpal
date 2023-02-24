37d1d0080cee3324d223d9dbdbf066c679443347
Taqi Raza <mttalpur@gmail.com>
2023-02-24 17:05:59 +0500

    everpal: use sparkos gapps

--- spark_everpal.mk
+++ spark_everpal.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common SparkOS stuff.
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-# Inherit Vendor GApps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
@@ -22,6 +19,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := spark_everpal
