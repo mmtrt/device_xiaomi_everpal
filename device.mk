d8ec468df01064ce1d8b73b2fd73130660f79735
Taqi Raza <mttalpur@gmail.com>
2023-03-12 15:47:26 +0500

    everpal: fix separator FM Radio

--- device.mk
+++ device.mk
@@ -480,9 +480,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
+
 # FM
 PRODUCT_PACKAGES += \
-    RevampedFMRadio
+    RevampedFMRadio \
     libmtkfmjni
 
 # USB
