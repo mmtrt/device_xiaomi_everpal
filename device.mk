d28f4300d11daad54f9343f3914d9d9c604ac8da
Taqi Raza <mttalpur@gmail.com>
2023-05-14 16:50:08 +0500

    everpal: device: use oss aidl vibrator hal

--- device.mk
+++ device.mk
@@ -365,6 +365,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.0.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.everpal
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy.everpal \
