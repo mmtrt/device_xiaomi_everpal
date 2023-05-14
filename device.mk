3dc0e5753ccc5940a17fdfa84c2ba2e9f560c793
Taqi Raza <mttalpur@gmail.com>
2023-05-14 16:47:29 +0500

    everpal: device: add media omx

--- device.mk
+++ device.mk
@@ -193,6 +193,9 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.example
 
 # Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
