0da482bec223e06abf9dfa2e56029b64b99d2d17
Taqi Raza <mttalpur@gmail.com>
2023-05-14 16:57:21 +0500

    everpal: device: drop extra radio hal deps

--- device.mk
+++ device.mk
@@ -265,14 +265,7 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.0.vendor \
-    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio@1.0.vendor \
-    android.hardware.radio@1.1.vendor \
-    android.hardware.radio@1.2.vendor \
-    android.hardware.radio@1.3.vendor \
-    android.hardware.radio@1.4.vendor \
     android.hardware.radio@1.5.vendor
 
 # Overlays
