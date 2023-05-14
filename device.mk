fe188271ab5a762392f782d4a887516828ced8b1
Taqi Raza <mttalpur@gmail.com>
2023-05-14 16:46:49 +0500

    everpal: device: drop extra gnss hal deps

--- device.mk
+++ device.mk
@@ -143,12 +143,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss.measurement_corrections@1.0.vendor \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@1.0.vendor \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.0.vendor \
     android.hardware.gnss@2.1.vendor
 
 # Health
