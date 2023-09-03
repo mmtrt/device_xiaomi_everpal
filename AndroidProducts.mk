28f4c76488991fa0a3000f50c15c4c2f5c8c8cb2
Taqi Raza <mttalpur@gmail.com>
2023-09-03 12:46:54 +0500

    everpal: add aosp build product

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_everpal.mk \
     $(LOCAL_DIR)/lineage_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
+    aosp_everpal-user \
+    aosp_everpal-userdebug \
+    aosp_everpal-eng \
     lineage_everpal-user \
     lineage_everpal-userdebug \
     lineage_everpal-eng
