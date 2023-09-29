530970a2290e6fb3a67349b05c3b2060dffa0994
Taqi Raza <mttalpur@gmail.com>
2023-09-29 20:08:25 +0500

    everpal: init alpha-droid
    everpal:

--- lineage_everpal.mk
+++ lineage_everpal.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit Vendor GApps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
@@ -22,6 +19,29 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS
+WITH_GAPPS := true
+TARGET_INCLUDE_GOOGLE_APP := false
+TARGET_INCLUDE_GOOGLE_CALCULATOR := true
+TARGET_INCLUDE_GOOGLE_CALENDAR := false
+TARGET_INCLUDE_GOOGLE_CHROME := false
+TARGET_INCLUDE_GOOGLE_DESKCLOCK := false
+TARGET_INCLUDE_GOOGLE_DRIVE := false
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := false
+TARGET_INCLUDE_GOOGLE_PHOTOS := false
+TARGET_INCLUDE_GOOGLE_SETUP := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+TARGET_DEFAULT_ADB_ENABLED := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_everpal
