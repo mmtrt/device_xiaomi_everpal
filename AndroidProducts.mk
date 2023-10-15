e14cdad2977b5d64c68fcda3b400db4b3a6566f2
Taqi Raza <mttalpur@gmail.com>
2023-10-15 23:25:44 +0500

    everpal: add arrow build product

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,6 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_everpal.mk \
     $(LOCAL_DIR)/aosp_everpal.mk \
     $(LOCAL_DIR)/lineage_everpal.mk
 
@@ -12,6 +13,9 @@ COMMON_LUNCH_CHOICES := \
     aosp_everpal-user \
     aosp_everpal-userdebug \
     aosp_everpal-eng \
+    arrow_everpal-user \
+    arrow_everpal-userdebug \
+    arrow_everpal-eng \
     lineage_everpal-user \
     lineage_everpal-userdebug \
     lineage_everpal-eng
